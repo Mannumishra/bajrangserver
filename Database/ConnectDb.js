af9b912c2e92ddf3327b5b3645cad98fdbe95806
mannumishra <mannumishra2207@outlook.com>
2024-08-08 12:20:17 +0530

    add chnages

--- Database/ConnectDb.js
+++ Database/ConnectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectdb = async()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/otp")
+        await mongoose.connect("mongodb+srv://mannu22072000:ddcQ0VARz2v8VGVi@cluster0.s92yp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Database connected successfully")
     } catch (error) {
         console.log(error)
