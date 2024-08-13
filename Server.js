f861747fe989612ccd3669111d8b0498a63e2c28
mannumishra <mannumishra2207@outlook.com>
2024-08-13 16:00:47 +0530

    update server.js

--- Server.js
+++ Server.js
@@ -4,18 +4,29 @@ const cors = require("cors");
 const router = require("./Routes/signupRoutes");
 const dotenv = require("dotenv");
 const slipRouter = require("./Routes/ReciptRouter");
+const path = require("path");
+
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 
-app.use(cors());
+
+// CORS configuration to allow requests from both your frontend domains
+const corsOptions = {
+    origin: ['https://bajrangvahinidal.com', 'https://www.bajrangvahinidal.com'], // Allow both domains
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies to be sent with the requests
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "Public" directory
-app.use(express.static("Public"));
-// app.use('/Public', express.static(path.join(__dirname, 'Public')));
+// app.use(express.static("Public"));
+app.use('/Public', express.static(path.join(__dirname, 'Public')));
 
 
 // Use the routers
