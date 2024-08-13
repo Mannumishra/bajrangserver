009ecd9cef6b1f52ac60026a196db8ac0da9fe2b
mannumishra <mannumishra2207@outlook.com>
2024-08-13 15:42:59 +0530

    update server.js

--- Server.js
+++ Server.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "Public" directory
-app.use(express.static("Public"));
+// app.use(express.static("Public"));
+app.use('/Public', express.static(path.join(__dirname, 'Public')));
+
 
 // Use the routers
 app.use("/api", router);
