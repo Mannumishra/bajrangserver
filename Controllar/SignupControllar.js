ce2829569754a1eb49b76c654bebb869e914c5af
mannumishra <mannumishra2207@outlook.com>
2024-08-13 17:19:24 +0530

    add chnages

--- Controllar/SignupControllar.js
+++ Controllar/SignupControllar.js
@@ -103,6 +103,92 @@ exports.signup = async (req, res) => {
 
     await user.save();
     if (paymentMethod === "Offline") {
+      const mailOptions = {
+        from: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+        to: user.email,
+        subject: 'Thank You for Joining Bajrang Vahini Dal',
+        html: `
+          <div style="max-width: 100%; padding: 20px; background-color: #e6a15c; border: 2px solid #c0440d; border-radius: 10px; margin: auto; box-sizing: border-box;">
+              <div style="text-align: center; margin-bottom: 20px;">
+                  <img src="https://bajrangvahinidal.com/static/media/1698868863_4b65b39f091f43c03be2.04be49a70e991ff03f4e.png" alt="बजरंग वाहिनी दल Logo" style="width: 100px; height: auto; margin-bottom: 10px;">
+                  <h1 style="font-size: 28px; margin: 0; color: #d02626;">बजरंग वाहिनी दल</h1>
+                  <p style="font-size: 14px; color: #000;">226, शटर वाली गली, गांव खानपुर, नई दिल्ली - 110062 (भारत)</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">श्री/श्रीमति:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.name}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">पता:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.address}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">दूरभाष:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.phone}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">ई मेल:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.email}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">माता/पिता का नाम:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.paranrsName}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">राशि (शब्दों में):</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.donationAmount}</p>
+              </div>
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">नकद/ड्राफ्ट/डिजिटल/चेक नं:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.paymentMethod}</p>
+              </div>
+              ${user.paymentMethod === 'Online' ? `
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">पेमेंट आईडी:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.razorpayOrderId}</p>
+              </div>` : ''}
+              <div style="margin-bottom: 15px;">
+                  <label style="font-size: 16px; color: #000;">आधार कार्ड नंबर:</label>
+                  <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.adharnumber}</p>
+              </div>
+              <div style="text-align: right; margin-top: 20px;">
+                  <img src="https://res.cloudinary.com/dsimn9z1r/image/upload/fl_preserve_transparency/v1723179469/WhatsApp_Image_2024-08-09_at_10.19.27-removebg_ozu41s.jpg?_s=public-apps" alt="Signature" style="width: 100px; height: auto; margin-bottom: 5px;">
+                  <p style="font-size: 18px; color: #000;">अधिकृत हस्ताक्षर</p>
+              </div>
+              <style>
+                @media (max-width: 600px) {
+                  .container {
+                    width: 100% !important;
+                    padding: 10px !important;
+                  }
+                  .container img {
+                    width: 100% !important;
+                    height: auto !important;
+                  }
+                  .container h1 {
+                    font-size: 24px !important;
+                  }
+                  .container p {
+                    font-size: 12px !important;
+                  }
+                  .container label {
+                    font-size: 14px !important;
+                  }
+                }
+              </style>
+          </div>
+        `
+      };
+
+      const adminMailOptions = {
+        from: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+        to: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+        subject: 'New Member Request And Receipt',
+        text: `A new member registration done successfully, please check the attachment of receipt.`,
+      };
+
+      await transporter.sendMail(mailOptions);
+      await transporter.sendMail(adminMailOptions);
       res.status(200).json({ success: true, message: 'User donation successful.' });
     } else {
       // Handle Online Payment
@@ -145,6 +231,92 @@ exports.paymentVerification = async (req, res) => {
     user.razorpayPaymentId = razorpay_payment_id;
     user.isPaid = true;
     await user.save();
+    const mailOptions = {
+      from: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+      to: user.email,
+      subject: 'Thank You for Joining Bajrang Vahini Dal',
+      html: `
+        <div style="max-width: 100%; padding: 20px; background-color: #e6a15c; border: 2px solid #c0440d; border-radius: 10px; margin: auto; box-sizing: border-box;">
+            <div style="text-align: center; margin-bottom: 20px;">
+                <img src="https://bajrangvahinidal.com/static/media/1698868863_4b65b39f091f43c03be2.04be49a70e991ff03f4e.png" alt="बजरंग वाहिनी दल Logo" style="width: 100px; height: auto; margin-bottom: 10px;">
+                <h1 style="font-size: 28px; margin: 0; color: #d02626;">बजरंग वाहिनी दल</h1>
+                <p style="font-size: 14px; color: #000;">226, शटर वाली गली, गांव खानपुर, नई दिल्ली - 110062 (भारत)</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">श्री/श्रीमति:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.name}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">पता:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.address}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">दूरभाष:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.phone}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">ई मेल:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.email}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">माता/पिता का नाम:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.paranrsName}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">राशि (शब्दों में):</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.donationAmount}</p>
+            </div>
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">नकद/ड्राफ्ट/डिजिटल/चेक नं:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.paymentMethod}</p>
+            </div>
+            ${user.paymentMethod === 'Online' ? `
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">पेमेंट आईडी:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.razorpayOrderId}</p>
+            </div>` : ''}
+            <div style="margin-bottom: 15px;">
+                <label style="font-size: 16px; color: #000;">आधार कार्ड नंबर:</label>
+                <p style="font-size: 14px; padding: 8px; background-color: #fff; border-radius: 4px;">${user.adharnumber}</p>
+            </div>
+            <div style="text-align: right; margin-top: 20px;">
+                <img src="https://res.cloudinary.com/dsimn9z1r/image/upload/fl_preserve_transparency/v1723179469/WhatsApp_Image_2024-08-09_at_10.19.27-removebg_ozu41s.jpg?_s=public-apps" alt="Signature" style="width: 100px; height: auto; margin-bottom: 5px;">
+                <p style="font-size: 18px; color: #000;">अधिकृत हस्ताक्षर</p>
+            </div>
+            <style>
+              @media (max-width: 600px) {
+                .container {
+                  width: 100% !important;
+                  padding: 10px !important;
+                }
+                .container img {
+                  width: 100% !important;
+                  height: auto !important;
+                }
+                .container h1 {
+                  font-size: 24px !important;
+                }
+                .container p {
+                  font-size: 12px !important;
+                }
+                .container label {
+                  font-size: 14px !important;
+                }
+              }
+            </style>
+        </div>
+      `
+    };
+
+    const adminMailOptions = {
+      from: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+      to: process.env.EMAIL_SEND || "bajrangvahinidal@gmail.com",
+      subject: 'New Member Request And Receipt',
+      text: `A new member registration done successfully, please check the attachment of receipt.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(adminMailOptions);
     res.status(200).json({ success: true, message: 'Payment verified and receipt sent.' });
   } catch (err) {
     console.error('Error verifying payment:', err);
